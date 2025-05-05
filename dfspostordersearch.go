ed2c643c3c213e9035de8bda9b970453c4f3932f
kavyasree2649 <76469755+kavyasree2649@users.noreply.github.com>
2025-05-05 14:11:03 +0530

    Create dfspostordersearch.go

---
+++ dfspostordersearch.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	values := []int{50, 30, 70, 20, 40, 60, 80, 10, 25, 35, 45}
+
+	var bt BinaryTree
+
+	// Insert multiple values
+	for _, val := range values {
+		bt.insert(&node{value: val})
+	}
+
+	list := bt.DFSPostorderSearch()
+	fmt.Println(list)
+}
+
+type node struct {
+	left  *node
+	right *node
+	value int
+}
+
+type BinaryTree struct {
+	root *node
+}
+
+func (bt *BinaryTree) DFSPostorderSearch() []int {
+	return traversePostorder(bt.root, []int{})
+}
+
+func (bt *BinaryTree) insert(nn *node) {
+	if bt.root == nil {
+		bt.root = nn
+		return
+	}
+	currentNode := bt.root
+	for {
+
+		if currentNode.value > nn.value {
+			//Left
+
+			if currentNode.left == nil {
+				currentNode.left = nn
+				return
+
+			}
+			currentNode = currentNode.left
+
+		} else {
+
+			//right
+			if currentNode.right == nil {
+				currentNode.right = nn
+				return
+			}
+			currentNode = currentNode.right
+		}
+	}
+
+}
+
+func traversePostorder(node *node, list []int) []int {
+	if node == nil {
+		return nil
+	}
+
+	if node.left != nil {
+		list = traversePostorder(node.left, list)
+	}
+
+	if node.right != nil {
+		list = traversePostorder(node.right, list)
+	}
+
+	list = append(list, node.value)
+
+	return list
+}
