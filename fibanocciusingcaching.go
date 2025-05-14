ea29166edc3472fb42bd461f6ddab7f486337808
kavyasree2649 <76469755+kavyasree2649@users.noreply.github.com>
2025-05-14 22:18:50 +0530

    Create fibanocciusingcaching.go

---
+++ fibanocciusingcaching.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+var calculations int
+var cacheMap = make(map[int]int)
+
+func main() {
+	out := fibanocii(1)
+	fmt.Println("sum: ", out)
+}
+
+func fibanocii(n int) int {
+	calculations++
+	if _, ok := cacheMap[n]; ok {
+		return cacheMap[n]
+	} else if n < 2 {
+		return n
+	} else {
+		cacheMap[n] = fibanocii(n-1) + fibanocii(n-2)
+		return cacheMap[n]
+	}
+
+}
